89fcd5e8f650a7dd2a47397b0bd7a18088ba0709
Ökkeş Akkurt <okkesakkurt46@gmail.com>
2024-03-10 23:02:56 +0300

    fixed part of references

--- client/src/components/home/PartOfReferences.jsx
+++ client/src/components/home/PartOfReferences.jsx
@@ -53,8 +53,12 @@ const PartOfReferences = () => {
 							slidesPerView: 2,
 							spaceBetween: 20,
 						},
-						1024: {
+						992: {
 							slidesPerView: 3,
+							spaceBetween: 20,
+						},
+						1200: {
+							slidesPerView: 4,
 							spaceBetween: 30,
 						},
 					}}>
@@ -63,14 +67,19 @@ const PartOfReferences = () => {
 							lazy={false}
 							key={logo._id}
 							className='mySwiper p-8'>
-							<div className='p-5 bg-white shadow-lg bg-opacity-70 shadow-slate-700 rounded-lg'>
-								<img
-									src={`http://localhost:3000/${logo.path.slice(43)}`}
-									className='rounded-lg w-full'
-									loading='lazy'
-								/>
-								<div>
-									<h3 className='text-xl sm:text-sm lg:text-xl my-3'>{logo.title}</h3>
+							<div className='p-5 bg-white shadow-lg bg-opacity-70 shadow-slate-700 rounded-lg flex-shrink-0 basis-auto flex-grow-0 min-h-[200px]'>
+								<div
+									className='relative w-full h-0'
+									style={{ paddingTop: '60%' }}>
+									<img
+										src={`http://localhost:3000/${logo.path.slice(43)}`}
+										className='absolute inset-0 w-full h-full object-contain rounded-lg'
+										loading='lazy'
+										alt={logo.title}
+									/>
+								</div>
+								<div className='flex flex-col items-center justify-center'>
+									<h3 className='text-xl sm:text-[1.1rem] lg:text-xl'>{logo.title}</h3>
 								</div>
 							</div>
 						</SwiperSlide>
