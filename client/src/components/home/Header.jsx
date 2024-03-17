ea6160d6e3388834594b80537f16384dd25fdc3f
Ökkeş Akkurt <okkesakkurt46@gmail.com>
2024-03-17 03:34:17 +0300

    first commit

--- client/src/components/home/Header.jsx
+++ client/src/components/home/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
 	useEffect(() => {
 		const fetchPhotos = async () => {
 			try {
-				const response = await axios.get('https://baskentapi.onrender.com/photos/all');
+				const response = await axios.get('http://localhost:3000/photos/all');
 				await setPhotos(response.data);
 			} catch (error) {
 				console.error('Error:', error);
@@ -40,7 +40,7 @@ const Header = () => {
 					<SwiperSlide key={photo._id}>
 						<div className='relative md:h-[400px] lg:h-[600px] h-[200px] sm:h-[400px] xl:h-[800px]'>
 							<img
-								src={`https://baskentapi.onrender.com/${photo.path.slice(43)}`}
+								src={`https://baskentapi.onrender.com/${photo.path.slice(42)}`}
 								alt={photo.title}
 								className='w-full'
 								loading='lazy'
